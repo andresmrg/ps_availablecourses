336af0b97eded2eb33a18575908415c2c46ccba8
andrewmrg <andrewramos@paradisosolutions.com>
2015-08-21 18:55:13 -0400

    available courses design inprogress

---
+++ block_ps_availablecourses.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once('/config.php');
+
+
+
+class block_ps_availablecourses extends block_list {
+
+	
+
+	public function init() {
+		$this->title = get_string('selfstudy','block_ps_availablecourses');
+	}
+
+	public function get_content() {
+		
+		global $DB, $OUTPUT;
+
+		if ($this->content !== null) {
+	      return $this->content;
+	    }
+
+        $this->content = new stdClass;
+        $this->content->items = array();
+        $this->content->icons = array();
+        
+        //get courses only copies type
+        $courses = $DB->get_records('block_ps_selfstudy_course', array ('course_type'=>'0','course_status'=>'0'), $sort='', $fields='*', $limitfrom=0, $limitnum=0);
+        
+        //on each course
+        foreach($courses as $key => $value) {
+        	//build url and built array of items
+        	$url = new moodle_url('/blocks/ps_availablecourses/managecourses.php');
+        	$this->content->items[] = html_writer::link($url, $value->course_name);
+        	//$value->course_name
+        	//print_object($value);
+        }
+        $this->content->footer = html_writer::link('/blocks/ps_availablecourses/allcourses.php', 'See full list', array('id' => 'psfootercenter'));
+        //$this->content->footer = html_writer::link('/blocks/ps_availablecourses/allcourses.php', 'See full list');
+
+	    return $this->content;
+	}   // Here's the closing bracket for the class definition
+
+	public function applicable_formats() {
+	  return array(
+	           'site-index' => true,
+	          'course-view' => true, 
+	   'course-view-social' => true,
+	                  'mod' => true, 
+	             'mod-quiz' => true
+	  );
+	}
+
+	public function instance_allow_multiple() {
+    return false;
+    }
+
+    //Allow configurations
+    function has_config() {
+        return false;
+    }
+
+}
---
+++ lang/en/block_ps_availablecourses.php
@@ -0,0 +1,14 @@
+<?php
+$string['pluginname'] = 'Self-Study Available Courses';
+$string['selfstudy'] = 'Self-Study Available Courses';
+$string['selfstudy:addinstance'] = 'Add a new Self-Study Available Courses block';
+$string['selfstudy:myaddinstance'] = 'Add a new Self-Study Available Courses block to the My Moodle page';
+$string['settings'] = 'Settings';
+$string['addcourse'] = 'Add a new self-study course';
+$string['link_managecourses'] = 'Manage self-study courses';
+$string['link_requests'] = 'View requests';
+$string['field_coursecode'] = 'Course Code';
+$string['field_coursename'] = 'Course Title';
+$string['field_description'] = 'Course Description';
+$string['field_checkbox'] = 'Create course link type';
+$string['field_checkbox_hide'] = 'Hide course';
