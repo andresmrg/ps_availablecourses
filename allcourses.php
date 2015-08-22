536293195e63c2ecd67bd5889769abb6332c8f45
andrewmrg <andresmao2@gmail.com>
2015-08-21 21:04:59 -0500

    New: View list of courses, link to see all courses, note to guide student, Page where all courses are displayed

---
+++ allcourses.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once('../../config.php');
+//require_once('course_form.php');
+//require_once('allcourses_form.php');
+
+global $DB, $OUTPUT, $PAGE;
+
+$context = context_system::instance();
+$PAGE->set_context($context);
+$PAGE->set_url('/blocks/ps_availablecourses/allcourses.php');
+$PAGE->set_pagelayout('standard');
+
+/*
+1. get records from db
+2. display records in the box, title, description and a button
+
+*/
+
+//get courses only copies type
+$courses = $DB->get_records('block_ps_selfstudy_course', array ('course_type'=>'0','course_status'=>'0'), $sort='', $fields='*', $limitfrom=0, $limitnum=0);
+
+$site = get_site();
+echo $OUTPUT->header();
+foreach($courses as $key => $value) {
+	echo $OUTPUT->box('
+		<div style="border:solid 1px #CCC; padding: 20px; margin-bottom: 10px;">
+			<strong>Course Title:</strong> '.$value->course_name.'<br><br>
+			<p>'.$value->course_description.'</p>
+			<a class="ps_request_button" href="#">Request Course</a>
+		</div>');
+}
+echo $OUTPUT->footer();
+
+
+
+// form didn't validate or this is the first display
--- block_ps_availablecourses.php
+++ block_ps_availablecourses.php
@@ -6,8 +6,6 @@ require_once('/config.php');
 
 class block_ps_availablecourses extends block_list {
 
-	
-
 	public function init() {
 		$this->title = get_string('selfstudy','block_ps_availablecourses');
 	}
@@ -24,18 +22,16 @@ class block_ps_availablecourses extends block_list {
         $this->content->items = array();
         $this->content->icons = array();
         
+        //introduction
+        $this->content->items[] = "<p>In this section you can find the list of all self-study courses. Click on the course for more information or click on See full list to display all courses.</p>";
+        
         //get courses only copies type
         $courses = $DB->get_records('block_ps_selfstudy_course', array ('course_type'=>'0','course_status'=>'0'), $sort='', $fields='*', $limitfrom=0, $limitnum=0);
-        
-        //on each course
         foreach($courses as $key => $value) {
-        	//build url and built array of items
-        	$this->content->items[] = '<div style="text-align:center"><a href="#">'.$value->course_name.'</a>';
-
+        	//build the list of courses
+        	$this->content->items[] = '<div><a href="#">'.$value->course_name.'</a></div>';
         }
-        $this->content->footer = '<div style="text-align:center"><a href="#">See full list</a></div>';
-        //$this->content->footer = html_writer::link('/blocks/ps_availablecourses/allcourses.php', 'See full list', array('id' => 'psfootercenter'));
-        //$this->content->footer = html_writer::link('/blocks/ps_availablecourses/allcourses.php', 'See full list');
+        $this->content->footer = '<br><div style="text-align:center"><a href="blocks/ps_availablecourses/allcourses.php">See full list</a></div>';
 
 	    return $this->content;
 	}   // Here's the closing bracket for the class definition
