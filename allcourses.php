9cf7e24ef3ba8c6a5186aea05e40ce6c9fc8786d
andrewmrg <andrewramos@paradisosolutions.com>
2015-09-25 14:04:34 -0400

    Filter search through GET method

--- allcourses.php
+++ allcourses.php
@@ -32,8 +32,12 @@ if($filterform->is_cancelled()) {
 
 } else if ($fromform = $filterform->get_data()) {
 
-	$sqlconditions = "course_code = '".$fromform->filter_code."' AND ";
-	$site = get_site();
+	$url = new moodle_url($CFG->wwwroot.'/blocks/ps_availablecourses/allcourses.php?code='.$fromform->filter_code);
+    redirect($url);
+
+    //commented this since the filter should be through get method.
+	//$sqlconditions = "course_code = '".$fromform->filter_code."' AND ";
+	/*$site = get_site();
 	echo $OUTPUT->header(); //output header
 	$filterform->display();
 	echo "<hr>";
@@ -44,9 +48,17 @@ if($filterform->is_cancelled()) {
 	$table->set_sql($fields, $from, $sqlconditions);
 	$table->define_baseurl("$CFG->wwwroot/blocks/ps_availablecourses/allcourses.php");
 	$table->out(30, true); //print table
-	echo $OUTPUT->footer();
+	echo $OUTPUT->footer();*/
 
 } else {
+
+	//if was filtered, look for the code.
+	if(isset($_GET['code'])) {
+		$course_code = $_GET['code'];
+		$sqlconditions = "course_code = '".$course_code."'";
+	} else {
+		$sqlconditions = '1';
+	}
 
 	$site = get_site();
 	echo $OUTPUT->header(); //output header
@@ -55,7 +67,7 @@ if($filterform->is_cancelled()) {
 	//sql to get all requests
 	$fields = '*';
 	$from = "{block_ps_selfstudy_course}";
-	$sqlconditions = '1';
+	//$sqlconditions = '1';
 	$table->set_sql($fields, $from, $sqlconditions);
 	$table->define_baseurl("$CFG->wwwroot/blocks/ps_availablecourses/allcourses.php");
 	$table->out(30, true); //print table
--- filter_form.php
+++ filter_form.php
@@ -16,8 +16,7 @@ class filter_form extends moodleform {
         $buttonarray[] =& $mform->createElement('submit', 'submit_button', get_string('submitbutton','block_ps_selfstudy'));
         $buttonarray[] =& $mform->createElement('cancel', 'cancel_button', get_string('resetbutton','block_ps_selfstudy'));
         $mform->addGroup($buttonarray, 'buttonarray', '', '', false);
-        //$mform->addElement('submit', 'save', get_string('submitbutton','block_ps_selfstudy')); 
-        //$mform->addElement('reset', 'reset_button', get_string('resetbutton','block_ps_selfstudy'));        
+      
     }
 
 }
